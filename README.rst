489c4bae3917c18971f1bda03acba32d50e7b294
Andrew Spiers <andrew@andrewspiers.net>
2014-07-22 19:09:45 +1000

    initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+puppet-reissue-certs
+====================
+Reissue puppet certs, explore how fabric works.
---
+++ fabfile.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+#TODO: fping task to check the node is there.
+
+#      possibly some task or check for connection?
+#      ---->nope, happy enough if fabric reports connection errors.
+#
+
+#TODO: make a task which calls puppet cert {list,clean,sign}, and convert
+#      the existing list_puppet_cert and clean_puppet_cert into wrappers.
+
+from fabric.api import env, hosts, run, settings, task
+
+import ConfigParser
+import os
+
+try:
+  configfile = os.environ['PUPPET-REISSUE-CERTS-RC']
+except KeyError:
+  configfile = 'puppet-reissue-certs.conf'
+
+config = ConfigParser.SafeConfigParser()
+config.read(configfile)
+
+
+nodefile = config.get('main','nodefile')
+
+env.roledefs['puppetmaster'] = config.get('main','puppetmaster')
+puppetuser = config.get('main','puppetuser')
+
+@task
+def get_nodes(filename=nodefile, prepend_user=False, user=puppetuser):
+  """given a file name, return a list of the nodes in that file.
+  prepend 'puppetuser@' to each node name. 'puppetuser' should be root.
+  """
+  out = list()
+  with open (filename, 'r') as f:
+    for line in f:
+      if len(line) > 1:
+        if '@' not in line and prepend_user:
+          out.append(user + '@' + line.split(',')[0])
+        else:
+          out.append(line.split(',')[0])
+  for i in out:
+    print (i)
+  print out  
+  return out
+
+
+@task
+def remove_puppet_ssl():
+  #settings imported from fabric.api. Do not abort on nonzero exit status.
+  with settings(warn_only=True):
+    #run imported from fabric.api
+    run('service puppet stop')
+    run('killall puppet')
+    run('rm -rf /var/lib/puppet/ssl')
+    run('puppet agent -t')
+
+@task
+@hosts(env.roledefs['puppetmaster'])
+def list_puppet_cert(qn=None):
+  if qn is None:
+    qn = env.querynode
+  #run 'puppet cert list $querynode' on the puppetmaster 
+  #querynode could/should be defined in the commmand line like
+  #fab --set querynode=host.example.com list_puppet_cert
+  command = "puppet cert list " + qn
+  run(command)
+
+@task
+@hosts(env.roledefs['puppetmaster'])
+def list_all_certs():
+  """do puppet cert list X, for X in get_nodes()
+  (which reads the nodes in from a file)"""
+  for n in get_nodes():
+    list_puppet_cert(n)
+
+@task
+@hosts(env.roledefs['puppetmaster'])
+def clean_puppet_cert(client=None):
+  """clean client's certificate from the puppetmaster
+  'fab --set clientnode=host.example.com clean_puppet_cert should
+  run 'puppet cert clean host.example.com on the puppetmaster'.
+  """
+  if client is None:
+    client = env.clientnode
+  command = "puppet cert clean " + client
+  run(command)
+  pass
+
+def main():
+  print (get_nodes())
+
+if __name__ == "__main__":
+  main()
