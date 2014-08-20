9f8947ee7d540da982478194708fbbfafedf0966
Andrew Spiers <andrew@andrewspiers.net>
2014-08-20 20:41:24 +1000

    start using get_unreported
    Scrape the website instead of relying on a pregenerated list of
    non-reporing nodes.

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,8 @@
 #TODO: make a task which calls puppet cert {list,clean,sign}, and convert
 #      the existing list_puppet_cert and clean_puppet_cert into wrappers.
 
+import get_unreported
+
 from fabric.api import env, hosts, run, settings, task
 
 import ConfigParser
@@ -38,11 +40,32 @@ def get_nodes(filename=nodefile, prepend_user=False, user=puppetuser):
   return out
 
 @task
+def get_unreported_nodes(prepend_user=True, user=puppetuser):
+  """use get_unreported.py to scrape the puppet dashboard to find those nodes
+  that are not reporting, return a list of them, if prepend_user, then
+  return the list as a list that can be used as connection strings by fabric.
+  """
+  unreported = get_unreported.get_unreported()
+  out = list()
+  if prepend_user and len(unreported) > 0:
+    for i in unreported:
+      out.append(user + '@' + i)
+  else:
+    out = unreported
+  return out
+
+
+@task
 @hosts(get_nodes(prepend_user=True))
 def get_puppet_status(user=puppetuser):
   with settings(warn_only=True):
     run('service puppet status')
 
+@task
+@hosts(get_unreported_nodes())
+def get_puppet_status2(user=puppetuser):
+  with settings(warn_only=True):
+    run('service puppet status')
 
 @task
 def remove_puppet_ssl():
